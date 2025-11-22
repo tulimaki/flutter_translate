38f3155496a111a81a8961b4c1282c1eef8fc6f8
Panu Tulimaki <tulimaki@gmail.com>
2025-11-22 19:19:51 +0200

    Fix for flutter 3.38

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,15 +1,12 @@
-class Constants
-{
-    static const String assetManifestFilename = 'AssetManifest.json';
+class Constants {
+  static const String localizedAssetsPath = 'assets/i18n';
 
-    static const String localizedAssetsPath = 'assets/i18n';
+  static const String pluralZero = 'zero';
+  static const String pluralOne = 'one';
+  static const String pluralTwo = 'two';
+  static const String pluralFew = 'few';
+  static const String pluralMany = 'many';
+  static const String pluralOther = 'other';
 
-    static const String pluralZero = 'zero';
-    static const String pluralOne = 'one';
-    static const String pluralTwo = 'two';
-    static const String pluralFew = 'few';
-    static const String pluralMany = 'many';
-    static const String pluralOther = 'other';
-
-    static const String pluralValueArg = '{{value}}';
+  static const String pluralValueArg = '{{value}}';
 }
